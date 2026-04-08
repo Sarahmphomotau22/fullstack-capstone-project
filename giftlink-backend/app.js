12d27e0f5c6358bca18e6ff720d068299a9a4c16
sarah mpho <sarahmphomotau22@gmail.com>
2026-04-08 17:03:43 +0200

    commit1

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,6 +22,13 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+app.use(pinoHttp({ logger }));
+
 // Gift API Task 1: import the giftRoutes and store in a constant called giftRoutes
 const giftRoutes = require('./routes/giftRoutes');
 
@@ -35,6 +42,10 @@ const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
 // Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
+
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
 
